de0754679aa944f2d05c79c1e60ef6b566686f7a
criverafranco <carlosmauriciorivera.riverafranco@blackboard.com>
2022-07-25 17:54:58 -0500

    removed unused user actions

--- src/challenges/Challenge3/actions/useCountryActions.ts
+++
@@ -1,7 +0,0 @@
-export const useCountryActions = () => {
-  const list = () => ({ type: "COUNTRY:LIST" });
-
-  return {
-    list,
-  };
-};
--- src/challenges/Challenge3/actions/useUserActions.ts
+++ src/challenges/Challenge3/actions/useUserActions.ts
@@ -1,16 +1,10 @@
 export const useUserActions = () => {
   const list = () => ({ type: "USER:LIST" });
 
-  const create = (payload) => ({ type: "USER:CREATE", payload });
-
   const update = (payload) => ({ type: "USER:UPDATE", payload });
-
-  const remove = (payload) => ({ type: "USER:REMOVE", payload });
 
   return {
     list,
-    create,
     update,
-    remove,
   };
 };
